fb09015e82b467f93953384f9ba0a47331da24b5
Jacob Simpson <jsimpson@email.sc.edu>
2020-03-27 17:06:57 -0400

    Management basic layout is set

---
+++ src/components/management/JobList.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h3>This will be the job list</h3>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
--- src/views/Management.vue
+++ src/views/Management.vue
@@ -1,11 +1,33 @@
 <template>
   <div class="management">
-    <h2>This is the Management page</h2>
+    <div class="row">
+      <div class="col-3 job-list">
+        <JobList />
+      </div>
+      <div class="col-9 job-info">
+        <h2>This is the Management page</h2>
+      </div>
+    </div>
+    <div class="job-info-modal">
+      <h3>This will be the popup modal</h3>
+    </div>
   </div>
 </template>
 
 <script>
+import JobList from '../components/management/JobList'
 export default {
-
+  components: {
+    JobList
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+  .job-list {
+    height: calc(100vh - 64px);
+  }
+
+  .job-info {
+  }
+</style>
